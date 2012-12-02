7f1bae215e68effe85cf3f069ed445368fd94662
Coleman Watts <coleman@civicrm.org>
2012-12-02 13:21:42 -0800

    node path edit improvements

--- woolman_website/node_form.js
+++ woolman_website/node_form.js
@@ -1,37 +1,54 @@
 (function ($, D) {
+
   $(document).ready(function() {
+    /**
+     * Callback when path alias is updated
+     */
     function editPath() {
       var site = $('#woolman-path-site').val();
       var path = $('#woolman-path-path').val();
-      var description = 'No url path set';
-      var url = '';
-      if (path.length > 0 || site.length > 0) {
-        if (path.charAt(0) == '/') {
+      var description = 'None';
+      if (path.length || site.length) {
+        // Limit to url-friendly characters
+        path = path
+          .toLowerCase()
+          .replace(/[+&]+/g, '+')
+          .replace(/[^a-z0-9.+\/]+/g, '-')
+          .replace(/(\/{2,}|\/[.\-+]+|[.\-+]\/+)/g, '/');
+        // Path must not start with a slash
+        while (path.charAt(0) == '/' || path.charAt(0) == '+' || path.charAt(0) == '.' || path.charAt(0) == '-') {
           path = path.substr(1);
         }
-        // Limit to url-friendly characters
-        path = path.toLowerCase().replace(/[^a-z0-9.\/]+/g, '-').replace('//', '/');
         $('#woolman-path-path').val(path);
         if (path.length > 0) {
-          var lastIndex = path.length - 1;
           // Allow a trailing slash while the user is typing but not in the final product
+          var lastIndex = path.length - 1;
           if (path.charAt(lastIndex) == '/') {
             path = path.substr(0, lastIndex);
           }
           // If both site and path exist, insert slash between them
-          if (site.length > 0 && path.length > 0) {
+          if (site.length && path.length) {
             site += '/';
           }
         }
-        description = 'http://' + $('#woolman-path-site option:selected').text() + '/' + path;
+        if (path.length || site.length) {
+          description = 'http://' + $('#woolman-path-site option:selected').text();
+          if (path.length) {
+            description += '/' + path;
+          }
+        }
       }
-      url = site + path;
+      // Display url
       $('#woolman-url').html(description);
       $('.vertical-tabs-list-path .summary').html(description.replace('http://', ''));
-      $('#edit-path').val(url);
+      // Set internal path
+      $('#edit-path').val(site + path);
     }
 
-    if ($('#edit-path').length > 0 && $('#edit-pathauto-perform-alias').length < 1) {
+    /**
+     * User-friendly way of editing node path alias
+     */
+    if ($('#edit-path').length && $('#edit-pathauto-perform-alias').length < 1) {
       var sites = {
         '': 'woolman.org',
         '~semester': 'semester.woolman.org',
@@ -39,11 +56,13 @@
         '~blog': 'blog.woolman.org',
       }
       $('#edit-path-wrapper *').hide();
-      $('#edit-path-wrapper').append('<div class="subset" id="woolman-path"></div>');
-      $('#edit-path-wrapper').append('<div class="description" id="woolman-url">None</div>');
+      $('#edit-path-wrapper').append(
+        '<div class="subset" id="woolman-path"></div>' +
+        '<div class="description" id="woolman-url">None</div>');
       var cont = $('#woolman-path');
-      cont.append('<div class="form-item"><label>Site</label><select id="woolman-path-site"></select></div>');
-      cont.append('<div class="form-item"><label>Path</label><input id="woolman-path-path" class="form-text" type="text"></input></div>');
+      cont.append(
+        '<div class="form-item"><label>Site</label><select id="woolman-path-site"></select></div>' +
+        '<div class="form-item"><label>Path</label><input id="woolman-path-path" class="form-text" type="text" size="32"></input></div>');
       for (var i in sites) {
         $('select', cont).append('<option value="'+i+'">' + sites[i] + '</option>');
       }
@@ -69,8 +88,10 @@
     }
   });
 
-  // Inline image rotation
-  D.behaviors.w_rot = function(context) {
+  /**
+   * Inline image rotation
+   */
+  D.behaviors.woolman_img_rot = function(context) {
     $(".insert select.insert-style", context).not(".rot").addClass("rot").change(function() {
       var img = $(this).parents(".filefield-element").find(".imagefield-preview img");
       img.removeClass("rotate-90-l").removeClass("rotate-90-r");
