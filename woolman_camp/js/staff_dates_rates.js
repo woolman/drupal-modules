204cde02ab2345048547929c35bd0a85dc218889
Coleman Watts <coleman@civicrm.org>
2012-12-08 13:46:01 -0800

    First steps toward camp abstraction

--- woolman_camp/js/staff_dates_rates.js
+++ woolman_camp/js/staff_dates_rates.js
@@ -3,38 +3,25 @@
   $(document).ready(function() {
     $('.container-inline div[id$="year-wrapper"]').css('display','none');
 
-    $('.number-of-sessions').change(function() {
+    $('.number-of-events').change(function() {
       var n = $(this).val();
-      if(n<6) {
-        $(this).parents('fieldset.session-selection').children('.session-dates').slice(n).hide(400);
+      var max = $('option:last', this).attr('value');
+      if (n < max) {
+        $(this).parent().parent().children('.session-dates, .shuttle-times').slice(n).hide(400);
       }
-      if(n>0) {
-        $(this).parents('fieldset.session-selection').children('.session-dates').slice(0,n).show(400);
+      if (n > 0) {
+        $(this).parent().parent().children('.session-dates, .shuttle-times').slice(0, n).show(400);
       }
     });
 
-    $('.number-of-sessions').each(function() {
+    $('.number-of-events').each(function() {
+      var max = $('option:last', this).attr('value');
       var n = $(this).val();
-      if(n<6) {
-        $(this).parents('fieldset.session-selection').children('.session-dates').slice(n).css('display','none');
-      }
-    });
-    $('.number-of-shuttles').change(function() {
-      var n = $(this).val();
-      if(n<9) {
-        $(this).parents('fieldset.shuttle-selection').children('.shuttle-times').slice(n).hide(400);
-      }
-      if(n>0) {
-        $(this).parents('fieldset.shuttle-selection').children('.shuttle-times').slice(0,n).show(400);
+      if (n < max) {
+        $(this).parent().parent().children('.session-dates, .shuttle-times').slice(n).css('display','none');
       }
     });
 
-    $('.number-of-shuttles').each(function() {
-      var n = $(this).val();
-      if(n<9) {
-        $(this).parents('fieldset.shuttle-selection').children('.shuttle-times').slice(n).css('display','none');
-      }
-    });
   });
 
 }(jQuery, Drupal));
