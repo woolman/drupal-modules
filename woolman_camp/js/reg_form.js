5eae7e4db5bbab7488d6c039454982c89b5f0fb4
Coleman Watts <coleman@civicrm.org>
2012-12-30 17:01:32 -0800

    Finish overhauling woolman_camp fees and sessions

--- woolman_camp/js/reg_form.js
+++ woolman_camp/js/reg_form.js
@@ -19,20 +19,22 @@ function tally() {
   var total = 0;
   $("#tab-nav li a").each(function() {
     var cid = $(this).attr("href");
-    var weeks = 0;
+    var sessions = 0;
     if($(cid+' input:checkbox').length != 0) {
-      if($(cid+' input:checkbox:checked').length != 0) {total += 1;}
+      if($(cid+' input:checkbox:checked').length != 0) {
+        total += 1;
+      }
       $(cid+' input:checkbox:checked').each(function() {
-        weeks += parseInt($(this).val());
+        sessions += 1;
         $(this).parents('.register-button').addClass('selected');
       });
-      if(weeks == 0) {
+      if(sessions == 0) {
         $(this).find('.registered-for').html('<span class="unselected-text">Click to Register</span><span class="selected-text">Choose sessions &gt;&gt;</span>');
         $(cid+' .questions-wrapper').hide();
-      }else if(weeks == 1) {
-        $(this).find('.registered-for').html('Registered for: <strong>1</strong> week');
+      }else if(sessions == 1) {
+        $(this).find('.registered-for').html('Registered for: <strong>1</strong> session');
       }else {
-        $(this).find('.registered-for').html('Registered for: <strong>'+weeks+'</strong> weeks');
+        $(this).find('.registered-for').html('Registered for: <strong>'+sessions+'</strong> sessions');
       }
     }
   });
